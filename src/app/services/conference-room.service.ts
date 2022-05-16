d9a1dff08d8a55840de593ecf0fd68cb7cbb6daa
Juliusz Gorzen <julek.gorzen@gmail.com>
2022-05-16 19:21:50 +0200

    feature(crrs): add conferenceRoom service endpoints

--- src/app/services/conference-room.service.ts
+++ src/app/services/conference-room.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {ConferenceRoom} from "../models/conference-room";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConferenceRoomService {
 
-  constructor() { }
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getConferenceRooms(): Observable<ConferenceRoom[]> {
+    return this.http.get<ConferenceRoom[]>(`${this.apiUrl}/conference-room//get/all`);
+  }
+
+  public getConferenceRoomsForOrganisation(organisationId: number): Observable<ConferenceRoom[]> {
+    return this.http.get<ConferenceRoom[]>(`${this.apiUrl}/conference-room/get/all-for-organisation/${organisationId}`);
+  }
+
+  public addOConferenceRoom(conferenceRoom: ConferenceRoom): Observable<ConferenceRoom> {
+    return this.http.post<ConferenceRoom>(`${this.apiUrl}/conference-room/create`, conferenceRoom);
+  }
+
+  public updateConferenceRoom(conferenceRoom: ConferenceRoom): Observable<ConferenceRoom> {
+    return this.http.put<ConferenceRoom>(`${this.apiUrl}/conference-room/create`, conferenceRoom);
+  }
+
+  public deleteConferenceRoom(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/conference-room/remove`);
+  }
 }
