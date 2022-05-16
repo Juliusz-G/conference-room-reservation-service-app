bae2d2aa47f9acc4fbfcd408e6d570e65b7aa1d6
gracho <gracho1221@wp.pl>
2022-05-16 09:29:13 +0200

    add create function in front with routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddReservationComponent } from './components/reservation/add-reservation/add-reservation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'add-reservation', component: AddReservationComponent},
+  {path: '', redirectTo: 'app-root', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
