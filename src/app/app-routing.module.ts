cfce931524019edfd4786ad7cb21dd9358b7587b
gracho <gracho1221@wp.pl>
2022-05-17 09:33:08 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddReservationComponent } from './components/reservation/add-reservation/add-reservation.component';
+import { UpdateReservationComponent } from './components/update-reservation/update-reservation.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
+import { ConferenceRoomComponent } from './components/conference-room/conference-room.component';
+import { OrganisationComponent } from './components/organisation/organisation.component';
+import { OrganisationAddComponent } from './components/organisation-add/organisation-add.component';
 
 const routes: Routes = [
-  {path: 'add-reservation', component: AddReservationComponent},
-  {path: '', redirectTo: 'app-root', pathMatch: 'full'}
-
+  {path: 'conference-room', component: ConferenceRoomComponent},
+  {path: 'conference-room/:conferenceRoomId/edit', component: ConferenceRoomEditComponent},
+  {path: 'conference-room/create', component: ConferenceRoomCreateComponent},
+  {path: 'conference-room/:conferenceRoomId/view', component: ConferenceRoomViewComponent},
+  {path: 'conference-room/:conferenceRoomId/view', component: ConferenceRoomViewComponent},
+  {path: 'ogranisation', component: OrganisationComponent},
+  {path: 'ogranisation-add', component: OrganisationAddComponent},
+  {path: 'add-reservation', component: ReservationComponent},
+  {path: 'update-reservation/:id', component: UpdateReservationComponent},
+  // {path: '', redirectTo: 'app-root', pathMatch: 'full'},
 ];
 
 @NgModule({
